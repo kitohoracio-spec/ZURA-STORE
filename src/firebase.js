75633d4321d18d73b99588c1b7f2be3dd53bb061
kitohoracio-spec <kitohoracio@gmail.com>
2026-06-17 22:32:44 +0100

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAozVaH56Tg6CRQ2NC3fwI-HsF8d12MQD4",
+  authDomain: "zura-store-f1bbe.firebaseapp.com",
+  projectId: "zura-store-f1bbe",
+  storageBucket: "zura-store-f1bbe.firebasestorage.app",
+  messagingSenderId: "103218966468",
+  appId: "1:103218966468:web:c1e0eb61bda3611ee3fe91"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
